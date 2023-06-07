82b0d92938eadc9a2e7baa8b2ec89f48bc382d47
sashaaleks72 <sashaaleks72@gmail.com>
2023-06-07 16:20:15 +0300

    merge

--- Web/react-app/src/components/Instructions.tsx
+++ Web/react-app/src/components/Instructions.tsx
@@ -1,5 +1,7 @@
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
+import { Button } from "react-bootstrap";
+import InsertDriveFileIcon from "@mui/icons-material/InsertDriveFile";
 
 const Instructions = (): JSX.Element => {
     return (
@@ -7,6 +9,19 @@ const Instructions = (): JSX.Element => {
             <Typography className="text-center" variant="h4">
                 Instructions
             </Typography>
+            <Box
+                sx={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                    border: "1px solid rgba(0, 0, 0, 0.3)",
+                    padding: "5px",
+                    width: "100%",
+                }}>
+                <InsertDriveFileIcon sx={{ fontSize: "3.5rem" }} />
+                <Box sx={{ display: "flex", alignItems: "center" }}>
+                    <Button variant="outline-primary">DOWNLOAD</Button>
+                </Box>
+            </Box>
         </Box>
     );
 };
